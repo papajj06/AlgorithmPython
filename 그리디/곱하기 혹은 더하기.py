4b901fb0ff45ee442a43f8a10106446a599afc23
kimsubeen <papajj06@naver.com>
2022-03-14 23:51:26 +0900

    [그리디] 곱하기 혹은 더하기

--- 그리디/곱하기 혹은 더하기.py
+++ 그리디/곱하기 혹은 더하기.py
@@ -5,3 +5,14 @@ Created on Mon Mar 14 22:45:54 2022
 @author: papajj06
 """
 
+s = input()
+result = int(s[0])
+for i in range(1,len(s)):
+    num = int(s[i])
+    if num>=0 or num<=1:
+        result+=num
+    else :
+        result*=num
+print(result)
+    
+        
